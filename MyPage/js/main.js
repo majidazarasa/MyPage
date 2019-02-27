4af03a2f9fabb653f4191c47235b36bde58a6313
majidazarasa <46866429+majidazarasa@users.noreply.github.com>
2019-02-27 20:42:53 +0330

    Add files via upload

---
+++ MyPage/js/main.js
@@ -0,0 +1,41 @@
+function ShowPersonalInfo(){
+    document.getElementById("P1").innerText="نام و نام خانوادگی:  مجید آذرآسا";
+    document.getElementById("P2").innerText="وضعیت خدمت سربازی:  دارای کارت پایان خدمت";
+    document.getElementById("P3").innerText="موبایل : 09122544476 – 09357530217";
+    document.getElementById("P4").innerText="آدرس ایمیل :  azarasa.majid@gmail.com";
+}
+function ShowWorkExperience()
+{
+    document.getElementById("P1").innerText=" شرکت دنا فراز صنعت (دناکو) از سال 1394 الی سال 1397";
+    document.getElementById("P2").innerText="تدریس در دانشگاههای آزاد و غیر انتفاعی به مدت 10 ترم";
+    document.getElementById("P3").innerText="شرکت مهندسین مشاور موننکو ایران از سال 1387 الی سال 1390";
+    document.getElementById("P4").innerText="شرکت مهندسین مشاور قدس نیرو از سال 1373 الی سال 1394";
+}
+function ShowPersonalSkills()
+{
+    document.getElementById("P1").innerText="برنامه نویسی به زبان های Microsoft Visual C# 2008 بانکهای اطلاعاتی Oracle 9i و SQL Server 2005";
+    document.getElementById("P2").innerText="میکروکنترلهای ARM ، AVR و PIC";
+    document.getElementById("P3").innerText="PLCهای S7-300 زیمنس";
+    document.getElementById("P4").innerText="SCADA ، اتوماسیون پستها ، RTU و نرم افزارهای PAS، EMS و DMS";
+}
+function ShowTrainingInfo()
+{
+    document.getElementById("P1").innerText="Microsoft Visual C# .NET 2008";
+    document.getElementById("P2").innerText="Oracle 9i Developer courses (SQL and PL/SQL)";
+    document.getElementById("P3").innerText="PLCهای S7-300 زیمنس";
+    document.getElementById("P4").innerText="Network + & CCNA (Cisco Certified Network Associate)";
+}
+function ShowLanguagesInfo()
+{
+    document.getElementById("P1").innerText="انگلیسی ( خواندن : عالی ، نوشتن : خوب ، مکالمه : متوسط )";
+    document.getElementById("P2").innerText="ترکی آذربایجانی ( خواندن : متوسط ، نوشتن : متوسط ، مکالمه : متوسط )";
+    document.getElementById("P3").innerText="";
+    document.getElementById("P4").innerText="";
+}
+function ShowContactMeInfo()
+{
+    document.getElementById("P1").innerText="موبایل : 09357530217";
+    document.getElementById("P2").innerText="موبایل :  09122544476";
+    document.getElementById("P3").innerText="ایمیل : azarasa.majid@gmail.com";
+    document.getElementById("P4").innerText="";
+}
